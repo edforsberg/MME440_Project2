d0993ddf8edf0c2778a2a52e662adc51491fac80
Erik Forsberg <erikforsberg95@hotmail.com>
2019-04-29 14:32:37 +0200

    fixed some more data sets

--- CreateDataFunctions.py
+++ CreateDataFunctions.py
@@ -57,12 +57,38 @@ def Data1():
 def Data2():
     nrFeatures = 2
     mislabelProportion = 0
-    nrClasses = 4
+    nrClasses = 3
     nrMislabelPoints = 40
-    lows = [[0, 0], [-3.5, 1.5], [5, 0], [0, -5]]
-    highs = [[3, 2], [0, 5], [6, 4], [6, 2]]
-    dataPoints = [10000, 2000, 2000, 500]
+    lows = [[0, 0], [5, 0], [0, -5]]
+    highs = [[6, 5], [0, 5], [6, 4]]
+    dataPoints = [10000, 2000, 2000]
 
     Xdat, Ydat = GenerateUniformData(lows, highs, dataPoints)
     return Xdat, Ydat
+
+def Data3():
+    nrFeatures = 2
+    mislabelProportion = 0
+    nrClasses = 6
+    nrMislabelPoints = 40
+    means = [[1, 3], [-6, 2], [-2, 6], [-5, 4], [-8,0], [4,3]]
+    sigma = [[1, 1.4], [1.1, 1.1], [0.9, 1.2], [0.7, 1.1], [1.1,1.2], [1.0,1.0]]
+    dataPoints = [10000, 2000, 2000, 500, 800, 3000]
+
+    Xdat, Ydat = GenerateGaussianData(means, sigma, dataPoints)
+    return Xdat, Ydat
+
+def Data4():    
+    nrVars = 2
+    radius = 6
+    nrClasses = 4
+    sigma = np.ones([nrClasses, nrVars])
+    means = np.ones([nrClasses, nrVars])
+    for i in range (0,nrClasses-1):
+        means[i,0] = np.cos(i*np.pi*2/nrClasses)*radius
+        means[i,1] = np.sin(i*np.pi*2/nrClasses)*radius
+    dataPoints = np.ones(1,nrClasses)*1000
+    Xdat, Ydat = GenerateGaussianData(means, sigma, dataPoints)
+    return Xdat, Ydat
+  
 
